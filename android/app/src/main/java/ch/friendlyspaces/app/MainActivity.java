6e8de8bab842000a285c692cf01537f6c3a81a20
Friendly <friendly@friendlyspaces.ch>
2026-02-27 07:35:53 +0000

    Harden Android status bar insets for native shell

--- android/app/src/main/java/ch/friendlyspaces/app/MainActivity.java
+++ android/app/src/main/java/ch/friendlyspaces/app/MainActivity.java
@@ -1,6 +1,10 @@
 package ch.friendlyspaces.app;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
@@ -8,6 +12,11 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        Window window = getWindow();
+        WindowCompat.setDecorFitsSystemWindows(window, true);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#1E52BA"));
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
     }
 }
